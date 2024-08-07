5d58bed1c8145c08af2de6750815c16d9cdc3ba6
callmexhj <yqzhouah@foxmail.com>
2024-08-07 15:01:49 +0800

    fix: ios textdecoder问题修复

--- systemConfig/messageSender.js
+++ systemConfig/messageSender.js
@@ -1,11 +1,17 @@
 export function genRequest(modelInfo, messages) {
   // console.log(modelInfo)
-  const { modelNameTrue, modelVersionTrue, config } = modelInfo
+  const {
+    modelNameTrue,
+    modelVersionTrue,
+    config
+  } = modelInfo
   let url = ''
   let header = {}
   let data = {}
   if (modelNameTrue === '通义千问') {
-    const { APIKey } = config
+    const {
+      APIKey
+    } = config
     url = 'https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions'
     header = {
       'Authorization': `Bearer ${APIKey}`,
@@ -28,22 +34,35 @@ export function genRequest(modelInfo, messages) {
 }
 
 export function handleRequest(res) {
-  const decoder = new TextDecoder('utf-8')
-  const responseString = decoder.decode(res)
+  // const decoder = new TextDecoder('utf-8')
+  const type = Object.prototype.toString.call(res)
+  let responseString
+  if (type === "[object Uint8Array]") {
+    responseString = decodeURIComponent(escape(String.fromCharCode(...res)))
+  } else if (res instanceof ArrayBuffer) {
+    // 将ArrayBuffer转换为Uint8Array
+    const uint8Array = new Uint8Array(res);
+    responseString = decodeURIComponent(escape(String.fromCharCode(...uint8Array)))
+  }
   // 按api接口格式截取字符串后转换成数组
   const jsonStringArray = responseString.split('data:')
   const contents = [];
   for (let i = 1; i < jsonStringArray.length; i++) {
+    // console.log(jsonStringArray) 
     const jsonItem = JSON.parse(jsonStringArray[i])
-    // console.log(jsonItem)
     const {
-      choices: [
-        { delta: { content } }
-      ]
+      choices: [{
+        delta: {
+          content
+        }
+      }]
     } = jsonItem
     contents.push(content)
     if (jsonItem.choices[0].finish_reason === 'stop') {
-      return { stop: true }
+      return {
+        stop: true,
+        data: contents.join('')
+      }
     }
   }
   const fullRes = contents.join('')
