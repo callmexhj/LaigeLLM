1a2325d4de6294905a87ad172ed41afc9c3541fd
callmexhj <yqzhouah@foxmail.com>
2024-08-07 15:02:21 +0800

    fix: 修复真机键盘遮挡问题

--- components/ChatInput/ChatInput.js
+++ components/ChatInput/ChatInput.js
@@ -30,27 +30,6 @@ Component({
       this.setData({
         inputValue: ''
       })
-    },
-
-    handleRequest(res) {
-      const decoder = new TextDecoder('utf-8')
-      const responseString = decoder.decode(res)
-      // 按api接口格式截取字符串后转换成数组
-      const jsonStringArray = responseString.split('data:')
-      const contents = [];
-      for (let i = 1; i < jsonStringArray.length; i++) {
-        const jsonItem = JSON.parse(jsonStringArray[i])
-        // console.log(jsonItem)
-        const {
-          choices: [
-            { delta: { content } }
-          ]
-        } = jsonItem
-        contents.push(content)
-        if (jsonItem.choices[0].finish_reason === 'stop') break
-      }
-      const fullRes = contents.join('')
-      return fullRes
     }
   }
 })
